085b8cf4705ee6cfde8d06463593d33b2f5715c6
Lisa Jiamsirioungkul <visakhalisajiam@gmail.com>
2020-08-11 19:39:40 -0500

    Updates

---
+++ precache-manifest.c93671690ce8e092a474eedfb5671a80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "157c54adaa9234b9d2e88d07a3159ecd",
+    "url": "/react-flashcards/index.html"
+  },
+  {
+    "revision": "1c85d6abe1fbb9296e48",
+    "url": "/react-flashcards/static/css/main.f91b8d53.chunk.css"
+  },
+  {
+    "revision": "74c1396e9e02cd959470",
+    "url": "/react-flashcards/static/js/2.26afdcef.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/react-flashcards/static/js/2.26afdcef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c85d6abe1fbb9296e48",
+    "url": "/react-flashcards/static/js/main.9f475841.chunk.js"
+  },
+  {
+    "revision": "33295c81c874b96bdd74",
+    "url": "/react-flashcards/static/js/runtime-main.3e2f485e.js"
+  }
+]);
